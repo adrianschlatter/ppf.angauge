2eaa17c440557f44ff0a672ca011c6791ee5ca8f
Adrian Schlatter <10478149+adrianschlatter@users.noreply.github.com>
2026-01-25 22:05:51 +0100

    add unittest for thermometer

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -8,12 +8,23 @@ from pathlib import Path
 from ppf.watermeter import cli
 
 
-def test_known_img(capfd):
+def test_known_img_watermeter(capfd):
     DATADIR = Path(__file__).parent / 'data'
     with patch('sys.argv',
                ['read_meter',
-                str(DATADIR / 'config.toml'),
+                str(DATADIR / 'config_watermeter.toml'),
                 str(DATADIR / '2025-08-20T00:00:10.059926+02:00.bmp')]):
         cli.main()
         out, err = capfd.readouterr()
-        assert '0.63317' in out.strip()
+        assert '0.6331' in out.strip() or '0.6332' in out.strip()
+
+
+def test_known_img_thermometer(capfd):
+    DATADIR = Path(__file__).parent / 'data'
+    with patch('sys.argv',
+               ['read_meter',
+                str(DATADIR / 'config_thermometer.toml'),
+                str(DATADIR / '2026-01-02T06:30:10.662715+01:00.bmp')]):
+        cli.main()
+        out, err = capfd.readouterr()
+        assert '49.2' in out.strip()
