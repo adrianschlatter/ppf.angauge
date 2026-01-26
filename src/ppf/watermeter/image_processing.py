1ef3309a3517daeb63ad31f5a9ed205898ffa8fb
Adrian Schlatter <10478149+adrianschlatter@users.noreply.github.com>
2026-01-26 13:58:32 +0100

    Remove VirtualImage class
    It implemented lazy cartesian to polar coordinate transformation
    (evaluation only when asked for a coordinate, not before).

--- src/ppf/watermeter/image_processing.py
+++ src/ppf/watermeter/image_processing.py
@@ -69,63 +69,6 @@ def to_polar(img: np.ndarray, n_r: int, n_theta: int,
     return img[Y, X]
 
 
-class VirtualImage:
-    """
-    A VirtualImage represents an image in polar coordinates
-
-    It behaves like a (read-only) 2D numpy array of booleans but it actually
-    computes its pixels on-the-fly from a given cartesian image.
-
-    Also, it accumulates a theta distribution of all pixel values accessed via
-    __getitem__.
-    """
-
-    def __init__(self, img: np.ndarray, n_r: int, n_theta: int,
-                 r_min: float, r_max: float, threshold: float):
-        self.img = img
-        self.h_half, self.w_half = 0.5 * img.shape[0], 0.5 * img.shape[1]
-        self.h_max, self.w_max = img.shape[0] - 1, img.shape[1] - 1
-        self.n_r, self.n_theta = n_r, n_theta
-        self.r_min, self.r_max = r_min, r_max
-        self.dr = (r_max - r_min) / self.n_r
-        self.dtheta = 2 * np.pi / self.n_theta
-        self.threshold = threshold
-
-        self.theta_distrib = np.zeros(n_theta, dtype='float')
-
-        # precompute sine and cosine tables:
-        theta = np.linspace(0, 2 * np.pi, n_theta, endpoint=False)
-        self.sine_table = np.sin(theta)
-        self.cosine_table = np.cos(theta)
-
-    def __getitem__(self, key: tuple[int, int]) -> bool:
-        i_r, i_theta = key
-
-        # convert polar pixel numbers to (r, theta):
-        r = i_r * self.dr + self.r_min
-
-        # convert polar to cartesian:
-        x = self.w_half + r * self.sine_table[i_theta]
-        y = self.h_half - r * self.cosine_table[i_theta]
-
-        # convert to pixel indices:
-        i_y = max(0, min(self.h_max, round(y)))
-        i_x = max(0, min(self.w_max, round(x)))
-
-        # lookup pixel in original image and convert to grayscale:
-        value = to_handscale(*self.img[i_y, i_x])
-
-        # accumulate theta distribution for std dev calculation:
-        self.theta_distrib[i_theta] += value
-
-        # return whether pixel is above threshold:
-        return value > self.threshold
-
-    @property
-    def shape(self) -> tuple[int, int]:
-        return (self.n_r, self.n_theta)
-
-
 def flood_fill(img: np.ndarray,
                points: set[tuple[int, int]]) -> np.ndarray[bool]:
 
