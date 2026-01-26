d7e97bb588bc751ec20f623025bde61139c7a264
Adrian Schlatter <10478149+adrianschlatter@users.noreply.github.com>
2026-01-26 14:23:14 +0100

    drop diagnostics.to_polar()

--- src/ppf/watermeter/diagnostics.py
+++ src/ppf/watermeter/diagnostics.py
@@ -2,33 +2,6 @@ from __future__ import annotations
 import numpy as np
 
 
-def to_polar(img_hand: np.ndarray) -> np.ndarray:
-    """
-    Convert hand image to polar coordinates.
-    """
-
-    r_max = min(img_hand.shape) / 2.
-    x_pixel = np.arange(img_hand.shape[1]) - img_hand.shape[1] / 2
-    y_pixel = img_hand.shape[0] / 2 - np.arange(img_hand.shape[0])
-    r2_pixel = x_pixel[None, :]**2 + y_pixel[:, None]**2
-
-    try:
-        c_x, c_y = cog(img_hand * (r2_pixel >= (0.35 * r_max)**2))
-    except ValueError:
-        raise ValueError("Failed to compute center of gravity of hand image")
-
-    mu_theta = np.arctan2(c_x, c_y)
-    r_min = 14/40 * r_max
-    r = np.linspace(r_min, r_max, img_hand.shape[0], endpoint=False)
-    theta = np.linspace(mu_theta - np.pi / 2, mu_theta + np.pi / 2,
-                        img_hand.shape[1])
-    X = r[:, None] * np.sin(theta[None, :]) + img_hand.shape[1] / 2
-    Y = img_hand.shape[0] / 2 - r[:, None] * np.cos(theta[None, :])
-    img_polar = img_hand[Y.astype(int), X.astype(int)]
-
-    return img_polar, theta, mu_theta
-
-
 def rgb_to_hsv(rgb: np.ndarray) -> np.ndarray:
     """
     Convert RGB values to HSV.
