1231d0639e8f97ea65768c0d9acea00d9e980779
Adrian Schlatter <10478149+adrianschlatter@users.noreply.github.com>
2026-01-30 16:22:10 +0100

    unittests for cli
    * test --multiplier
    * test --hands
    * test overexposed image

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -20,6 +20,32 @@ def test_known_img_watermeter(capfd):
         assert '0.6331' in out.strip() or '0.6332' in out.strip()
 
 
+def test_multiplier_arg(capfd):
+    DATADIR = Path(__file__).parent / 'data'
+    with patch('sys.argv',
+               ['read_gauge',
+                '--multiplier',
+                '0.0001',
+                str(DATADIR / 'config_watermeter.toml'),
+                str(DATADIR / '2025-08-20T00:00:10.059926+02:00.bmp')]):
+        cli.main()
+        out, err = capfd.readouterr()
+        assert '0.6331' in out.strip() or '0.6332' in out.strip()
+
+
+def test_hands_arg(capfd):
+    DATADIR = Path(__file__).parent / 'data'
+    with patch('sys.argv',
+               ['read_gauge',
+                '--hands',
+                str(DATADIR / 'config_watermeter.toml'),
+                str(DATADIR / '2025-08-20T00:00:10.059926+02:00.bmp')]):
+        cli.main()
+        out, err = capfd.readouterr()
+        assert '1.89, 3.24, 3.34, 6.31, 0.468, 0.430, 0.453, 0.437' \
+            in out.strip()
+
+
 def test_known_img_thermometer(capfd):
     DATADIR = Path(__file__).parent / 'data'
     with patch('sys.argv',
@@ -51,3 +77,15 @@ def test_invalid_img(capfd):
 
         with pytest.raises(ValueError):
             cli.main()
+
+
+def test_unprocessable_img(capfd):
+    """Test an "all black" image not having a single hand pixel."""
+    DATADIR = Path(__file__).parent / 'data'
+    with patch('sys.argv',
+               ['read_gauge',
+                str(DATADIR / 'config_watermeter.toml'),
+                str(DATADIR / '2025-07-07T22:35:05.261227+02:00.bmp')]):
+        cli.main()
+        out, err = capfd.readouterr()
+        assert ' nan' in out.strip()
